7206904b4640a5d4432e05bda32b5689e9e5d713
ikumi-shimoda <ikm56976832@gmail.com>
2025-03-27 18:26:23 +0900

    pythonとapi連携

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,6 +13,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class NameRequest(BaseModel):
+    name: str
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to KoiLog API"} 
+
+@app.post("/api/")
+async def process_data(request: NameRequest):
+    print(request)
+    return {"name": request.name}
